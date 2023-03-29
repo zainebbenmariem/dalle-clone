d702c354c5fa8a9ddd534c5db9e1265740c965f1
unknown <zeinebbenmeriem2@gmail.com>
2023-03-29 15:54:59 +0100

    upload images to cloudinary

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,6 +8,12 @@ import Post from '../mongodb/modules/post.js';
 dotenv.config();
 
 const router = express.Router();
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+
+});
 
 export default router;
 
