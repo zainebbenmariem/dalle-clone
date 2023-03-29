35ae3a31829fc7f6838291821aa8eaf9a2c62f64
unknown <zeinebbenmeriem2@gmail.com>
2023-03-29 16:54:48 +0100

    create post routes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,6 +16,33 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
   const [searchText,setSearchText] = useState('');
 
+  useEffect(() => {
+    const fetchPosts = async() => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post',{
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if(response.ok){
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+
+      } catch (error) {
+        alert(error)
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPosts();
+  },[]); 
+
   return (
     <section max-w-7xl mx-auto>
       <div>
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -15,5 +15,35 @@ cloudinary.config({
 
 });
 
+// GET ALL POSTS
+
+router.route('/').get(async(req,res) => {
+    try {
+        const posts = await Post.find({});
+        res.status(200).json({ success: true, data: posts });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error });
+    }
+});
+
+// CREATE A POST
+
+router.route('/').post(async(req,res) => {
+    try {
+        const { name, prompt, photo } = req.body;
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        const newPost = await Post.create({
+        name,
+        prompt,
+        photo: photoUrl.url,
+    })
+
+        res.status(201).json({ success: true, data: newPost});
+    } catch (error) {
+        res.status(500).json({ success: false, message:error})
+    }
+});
+
 export default router;
 
